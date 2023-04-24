edf2ebe3e973bc9b73bcbfe6183bb97f30ea5164
Yujie Wang <79264663+Christol-Jalen@users.noreply.github.com>
2023-04-25 00:13:32 +0100

    Version 1.1

--- Plot_Amplifier.m
+++ Plot_Amplifier.m
@@ -1,9 +1,9 @@
 %t_row = 1:1:130432;
-t_row = linspace(0, 0.00246,130432);%fine tune 0.0025 to get better results
+t_row = linspace(0, 0.00246,130432);
 t = t_row';
 
-Inv_P_In = csvread("Inv_PrototypeFake_In.csv",0,1);
-Inv_P_Out = csvread("Inv_PrototypeFake_Out.csv",0,1);
+Inv_P_In = csvread("Inv_Prototype_In.csv",0,1);
+Inv_P_Out = csvread("Inv_Prototype_Out.csv",0,1);
 Inv_N_In = csvread("Inv_Nonswitched_In.csv",0,1);
 Inv_N_Out = csvread("Inv_Nonswitched_Out.csv",0,1);
 
@@ -13,9 +13,9 @@ Noninv_N_In = csvread("Noninv_Nonswitched_In.csv",0,1);
 Noninv_N_Out = csvread("Noninv_Nonswitched_Out.csv",0,1);
 
 Int_P_In = csvread("Integrator_Prototype_In.csv",0,1);
-Int_P_Out = csvread("NEW_INT_OUTPUT2.csv",0,1);
-Int_N_In = csvread("NEW_INT_INPUT.csv",0,1);
-Int_N_Out = csvread("NEW_INT_OUTPUT1.csv",0,1);
+Int_P_Out = csvread("Integrator_Prototype_Out.csv",0,1);
+Int_N_In = csvread("Integrator_Nonswitched_In.csv",0,1);
+Int_N_Out = csvread("Integrator_Nonswitched_Out.csv",0,1);
 
 
 %inverting amplifier
@@ -44,13 +44,12 @@ xlabel("Time(s)")
 ylabel("Voltage(V)")
 hold off 
 
-
 %integrator
 figure
 hold on
 plot(t, Int_N_In, "Linewidth", 1)
 plot(t, Int_N_Out,"green", "Linewidth", 1)
-plot (t, Int_P_Out, "red","Linewidth", 1) %-3990, -0.137: making fake
+plot (t, Int_P_Out, "red","Linewidth", 1) 
 grid on
 legend("Vin", "Non-Switched", "Prototype")
 title("Prototype Integrator Evaluation")
